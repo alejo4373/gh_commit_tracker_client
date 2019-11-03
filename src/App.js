72c8e79b0f09da7ed9db11fb6bb65a4222580eae
Alejo <alejandrofranco@pursuit.org>
2019-11-03 07:52:57 -0500

    make network request to get commits with axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+const API_BASE_URL = "http://localhost:3100"
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.commits = []
+  }
+
+  componentDidMount() {
+    this.getPushedCommits();
+  }
+
+  async getPushedCommits() {
+    try {
+      let { data } = await axios.get(`${API_BASE_URL}/commits`)
+      this.setState({
+        commits: data.payload
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.commits.map(commit =>
+            <div>
+              {commit.sha}
+            </div>
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
